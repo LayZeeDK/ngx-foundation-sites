ace4279040d67c21d796c813e1e606b9f5022a4d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-07-07 00:43:47 +0200

    chore: scaffold example app and e2e projects

---
+++ src/examples/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ex';
+}
