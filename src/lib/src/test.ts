14a087b3ecf92e3397574ec46eab0a23f84d5d9b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-07-07 02:29:40 +0200

    chore: use Headless Chrome for tests

--- src/lib/src/test.ts
+++ src/lib/src/test.ts
@@ -5,7 +5,23 @@
 // This file is required by karma.conf.js and loads recursively all the .spec
 // and framework files.
 
-import 'zone.js/dist/zone-testing';
+// Used for reflect-metadata in JIT. If you use AOT (and only Angular
+// decorators), you can remove.
+// Required for Augury, Karma, and Protractor.
+import 'core-js/es7/reflect';
+
+// Zone JS is required by default for Angular itself.
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+/*
+* In development mode, to ignore zone related error stack frames such as
+* `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+* import the following file, but please comment it out in production mode
+* because it will have performance impact when throw error
+*/
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+import 'zone.js/dist/zone-testing'; // Included with Angular CLI.
 
 import { getTestBed } from '@angular/core/testing';
 import {
