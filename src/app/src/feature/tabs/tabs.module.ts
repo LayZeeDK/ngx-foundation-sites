92130a56c2f45544e6172fef9726f81da2d66052
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-01-06 21:28:42 +0100

    docs(demo): fix import

--- src/app/src/feature/tabs/tabs.module.ts
+++ src/app/src/feature/tabs/tabs.module.ts
@@ -1,8 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FasTabsModule } from 'ngx-foundation-sites';
 
-import { CommonModule } from '../../../../../node_modules/@angular/common';
 import { BasicsComponent } from './basics';
 import { TabsPageComponent } from './tabs-page';
 import { tabsRoutes } from './tabs.routes';
@@ -18,4 +18,4 @@ import { tabsRoutes } from './tabs.routes';
     FasTabsModule,
   ],
 })
-export class TabsModule {}
+export class TabsModule { }
