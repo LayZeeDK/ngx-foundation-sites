b442f665b844da83c14d5ebd6b84e3a65411e3a6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-04-19 22:31:19 +0200

    docs(tabs): complete Basics feature

--- packages/example-app/containers/feature-tabs/src/lib/basics.component.ts
+++ packages/example-app/containers/feature-tabs/src/lib/basics.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FasTabsModule } from 'ngx-foundation-sites';
 
 @Component({
@@ -7,6 +8,13 @@ import { FasTabsModule } from 'ngx-foundation-sites';
   styles: [``],
   template: `
     <h2>Basics</h2>
+
+    <p>
+      There are two pieces to a tabbed interface: the tabs themselves, and the
+      content for each tab. The tabs are an element with a title. Each tab
+      contains a link to a tab. The unique ID of each tab will will match the
+      href of a tabstrip.
+    </p>
 
     <fas-tabs>
       <fas-tab id="panel1" title="Tab 1" [isActive]="true">
@@ -27,6 +35,44 @@ import { FasTabsModule } from 'ngx-foundation-sites';
         </p>
       </fas-tab>
     </fas-tabs>
+
+    <p>Put it all together, and we get this:</p>
+
+    <fas-tabs>
+      <fas-tab id="panel1" title="Tab 1" [isActive]="true">
+        <p>one</p>
+        <p>Check me out! I'm a super cool Tab panel with text content!</p>
+        <p>
+          <a routerLink="./" fragment="">I am a link but don't do anything</a>
+        </p>
+      </fas-tab>
+
+      <fas-tab id="panel2" title="Tab 2">
+        <p>two</p>
+        <textarea></textarea>
+        <button class="button">I do nothing!</button>
+      </fas-tab>
+
+      <fas-tab id="panel3" title="Tab 3">
+        <p>three</p>
+        <p>Check me out! I'm a super cool Tab panel with text content!</p>
+      </fas-tab>
+
+      <fas-tab id="panel3" title="Tab 4">
+        <p>four</p>
+        <img class="thumbnail" src="/assets/img/generic/rectangle-2.jpg" />
+      </fas-tab>
+
+      <fas-tab id="panel3" title="Tab 5">
+        <p>five</p>
+        <p>Check me out! I'm a super cool Tab panel with text content!</p>
+      </fas-tab>
+
+      <fas-tab id="panel3" title="Tab 6">
+        <p>six</p>
+        <img class="thumbnail" src="/assets/img/generic/rectangle-8.jpg" />
+      </fas-tab>
+    </fas-tabs>
   `,
 })
 export class TabsBasicsComponent {}
@@ -34,6 +80,6 @@ export class TabsBasicsComponent {}
 @NgModule({
   declarations: [TabsBasicsComponent],
   exports: [TabsBasicsComponent],
-  imports: [FasTabsModule],
+  imports: [RouterModule, FasTabsModule],
 })
 export class TabsBasicsScam {}
