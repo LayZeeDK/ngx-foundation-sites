9aaf41b244f045c92a917c57b15fcef85b2c1935
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-03-24 22:46:28 +0100

    chore: [nx migration] update-jest-config-extensions

--- packages/example-app/example-app/jest.config.ts
+++ packages/example-app/example-app/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 module.exports = {
   displayName: 'example-app',
   preset: '../../../jest.preset.js',
--- packages/ngx-foundation-sites/jest.config.ts
+++ packages/ngx-foundation-sites/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 module.exports = {
   displayName: 'ngx-foundation-sites',
   preset: '../../jest.preset.js',
