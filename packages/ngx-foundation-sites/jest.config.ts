a97c1a3a6c51ea29b241ba91caf4a76d0e07a9c1
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-03-24 22:46:28 +0100

    chore: [nx migration] update-to-export-default

--- packages/ngx-foundation-sites/jest.config.ts
+++ packages/ngx-foundation-sites/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
   displayName: 'ngx-foundation-sites',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
