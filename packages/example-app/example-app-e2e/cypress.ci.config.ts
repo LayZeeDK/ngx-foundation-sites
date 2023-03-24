a5356970f1bc2da0858d6ef90583c9ab2e86ec4f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-03-25 00:30:12 +0100

    chore: migrate configurations to Cypress 11 compatibility

--- packages/example-app/example-app-e2e/cypress.ci.config.ts
+++ packages/example-app/example-app-e2e/cypress.ci.config.ts
@@ -1,26 +1,15 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
-import setupNodeEvents from './src/plugins/index';
+import { getPreprocessorConfig } from '@jscutlery/cypress-harness/preprocessor-config';
 
-const cypressJsonConfig = {
-  chromeWebSecurity: false,
-  env: {
-    tsConfig: 'tsconfig.json',
-  },
-  fileServerFolder: '.',
-  fixturesFolder: './src/fixtures',
-  screenshotOnRunFailure: false,
-  video: false,
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  watchForFileChanges: false,
-  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  supportFile: 'src/support/e2e.ts',
-};
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname),
-    ...cypressJsonConfig,
-    setupNodeEvents,
+    screenshotOnRunFailure: false,
+    video: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    watchForFileChanges: false,
+    ...getPreprocessorConfig(),
   },
 });
